f72b423a4bfa52f53c111cf46af900e3a7b5e083
zawhtetnaing10 <zawgyi.gog@gmail.com>
2020-02-22 09:30:24 +0630

    Implemented Delegate

--- app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/mvp/presenters/MainPresenter.kt
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/mvp/presenters/MainPresenter.kt
@@ -1,5 +1,7 @@
 package com.padcmyanmar.padcx.padc_x_recyclerview_ypst.mvp.presenters
 
-interface MainPresenter {
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.delegates.NewsItemDelegate
+
+interface MainPresenter : NewsItemDelegate {
     fun onUiReady()
 }
